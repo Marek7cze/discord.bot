b5d7004b976cf0d924e81b3b22749b5140658032
Marek7cze <marek.stefke@gmail.com>
2026-02-24 20:58:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
 
 intents = discord.Intents.default()
 intents.members = True
@@ -8,12 +9,13 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"cogs.{filename[:-3]}")
+async def main():
+    # Load cogs
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user} is online!")
+    # Start bot
+    await bot.start(os.getenv("DISCORD_TOKEN"))
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+asyncio.run(main())
