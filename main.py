74eb74b4320562ef10aa4998ebf4dc2e6249aab4
Marek7cze <marek.stefke@gmail.com>
2026-02-24 20:50:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online!")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
