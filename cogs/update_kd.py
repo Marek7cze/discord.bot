702742ac02aab0f31bf898ae0d4ae5110a7ad8ab
Marek7cze <marek.stefke@gmail.com>
2026-02-24 21:46:06 +0100

    Update update_kd.py

--- cogs/update_kd.py
+++ cogs/update_kd.py
@@ -3,17 +3,16 @@ from discord.ext import commands
 from discord import app_commands
 import sqlite3
 
-GUILD_ID = 1247900579586642021
 conn = sqlite3.connect("player_stats.db")
 c = conn.cursor()
-
-def update_player(standoff_id, field, value):
-    c.execute(f"UPDATE players SET {field} = ? WHERE standoff_id = ?", (value, standoff_id))
-    conn.commit()
 
 def get_player(standoff_id):
     c.execute("SELECT * FROM players WHERE standoff_id = ?", (standoff_id,))
     return c.fetchone()
+
+def update_player(standoff_id, field, value):
+    c.execute(f"UPDATE players SET {field} = ? WHERE standoff_id = ?", (value, standoff_id))
+    conn.commit()
 
 class UpdateKD(commands.Cog):
     def __init__(self, bot):
